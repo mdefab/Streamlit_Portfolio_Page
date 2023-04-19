f5cf8da6c99403da0315a66ab1cedd46401b2223
mdefab <defab@live.ca>
2023-04-19 17:21:17 -0400

    Add project content to page

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -19,6 +20,31 @@ with col2:
     Yourself
     """
     st.write(content)
+
+
+st.write("Below you can find some of the apps I have built. Feel free to contact me!")
+
+
+col3, col4 = st.columns(2)
+
+
+df = pandas.read_csv("data.csv", sep=";")
+middle_index = (len(df) // 2) + 1
+
+with col3:
+    for index, row in df[0:middle_index].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.write(row['url'])
+        st.image(row['image'])
+
+with col4:
+    for index, row in df[middle_index:].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.write(row['url'])
+        st.image(row['image'])
+
 
 
 # use expanders to hide/show more description for each app
