35e786b6b8b83c7ccca7cd56320ecbd99637e6b4
mdefab <defab@live.ca>
2023-04-20 16:34:57 -0400

    Add form to contact page

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.header("Contact Me")
+
+with st.form(key="contact_form"):
+    st.text_input(label="Please enter your email address:", key="email")
+    st.text_area(label="Your Message:", key="description")
+    submit = st.form_submit_button(label="submit")
+
+    if "submit":
+        print(f"{st.session_state.email} wants to know {st.session_state.description}")
